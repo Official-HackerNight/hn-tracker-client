f102872b2a5ca3cdb91c2da0946f705e2c171493
Steven Kelsey <skelsey212@gmail.com>
2019-04-23 22:03:54 -0400

    project name change

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'TempAngular7'`, () => {
+  it(`should have as title 'HNTrackerClient'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('TempAngular7');
+    expect(app.title).toEqual('HNTrackerClient');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to TempAngular7!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to HNTrackerClient!');
   });
 });
